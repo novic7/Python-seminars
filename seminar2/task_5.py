36d63428edad7d27ee3eba9ea6e443211e2d2627
Anton Novikov <novic77@inbox.ru>
2022-09-27 15:22:59 +0900

    homework5

---
+++ seminar2/task_5.py
@@ -0,0 +1,7 @@
+#Реализовать алгоритм перемешивания списка.
+
+import random
+lst = [random.randint(0,10) for i in range(random.randint(5,20))]
+print(f"Исходный список:\n {lst}")
+random.shuffle(lst)
+print(f"Список после перемешивания:\n{lst}")
